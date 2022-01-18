c836fc4fffca55fb5668718f8fa1be3ce630e8fc
Anurag Soni <anurag@sonianurag.com>
2022-01-17 22:30:49 -0500

    log serialized response in test

--- http/test/test_shuttle_http.ml
+++ http/test/test_shuttle_http.ml
@@ -11,19 +11,10 @@ let pipe () =
 ;;
 
 let test_post_req_with_fixed_body =
-  "POST /wp-content/uploads/2010/03/hello-kitty-darth-vader-pink.jpg HTTP/1.1\r\n\
-   Host: www.kittyhell.com   \r\n\
-   User-Agent: Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; ja-JP-mac; rv:1.9.2.3) \
-   Gecko/20100401 Firefox/3.6.3 Pathtraq/0.9\r\n\
-   Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n\
-   Accept-Language: ja,en-us;q=0.7,en;q=0.3\r\n\
-   Accept-Encoding: gzip,deflate\r\n\
-   Accept-Charset: Shift_JIS,utf-8;q=0.7,*;q=0.7\r\n\
+  "POST /hello HTTP/1.1\r\n\
+   Host: www.example.com   \r\n\
    Content-Length: 5\r\n\
    Connection: close\r\n\
-   Cookie: wp_ozh_wsa_visits=2; wp_ozh_wsa_visit_lasttime=xxxxxxxxxx; \
-   __utma=xxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.x; \
-   __utmz=xxxxxxxxx.xxxxxxxxxx.x.x.utmccn=(referral)|utmcsr=reader.livedoor.com|utmcct=/reader/|utmcmd=referral\r\n\
    \r\n\
    Hello\r\n"
 ;;
@@ -36,34 +27,29 @@ let%expect_test "test simple server" =
     let%bind () =
       Pipe.iter_without_pushback body ~f:(fun v -> Writer.write_line stdout v)
     in
-    Writer.writef stdout !"%{sexp: Cohttp.Request.t}" req;
-    Server.respond_string "Hello"
+    Writer.writef stdout !"%{sexp: Cohttp.Request.t}\n" req;
+    Server.respond_string "World"
   in
-  let%bind reader, writer = pipe () in
+  let%bind reader, write_to_reader = pipe () in
+  let%bind read_from_writer, writer = pipe () in
+  let reader_pipe = Input_channel.pipe read_from_writer in
   let finished = Ivar.create () in
   (Shuttle_http.Server.run_server_loop handler reader writer
   >>> fun () -> Ivar.fill finished ());
-  Output_channel.write writer test_post_req_with_fixed_body;
-  Output_channel.schedule_flush writer;
+  Output_channel.write write_to_reader test_post_req_with_fixed_body;
+  Output_channel.schedule_flush write_to_reader;
   let%bind () = Ivar.read finished in
   let%bind () =
     [%expect
       {|
     Hello
-    ((headers
-      ((Host www.kittyhell.com)
-       (User-Agent
-        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; ja-JP-mac; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 Pathtraq/0.9")
-       (Accept "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-       (Accept-Language "ja,en-us;q=0.7,en;q=0.3") (Accept-Encoding gzip,deflate)
-       (Accept-Charset "Shift_JIS,utf-8;q=0.7,*;q=0.7") (Content-Length 5)
-       (Connection close)
-       (Cookie
-        "wp_ozh_wsa_visits=2; wp_ozh_wsa_visit_lasttime=xxxxxxxxxx; __utma=xxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.x; __utmz=xxxxxxxxx.xxxxxxxxxx.x.x.utmccn=(referral)|utmcsr=reader.livedoor.com|utmcct=/reader/|utmcmd=referral")))
-     (meth POST) (scheme ())
-     (resource /wp-content/uploads/2010/03/hello-kitty-darth-vader-pink.jpg)
-     (version HTTP_1_1) (encoding (Fixed 5))) |}]
+    ((headers ((Host www.example.com) (Content-Length 5) (Connection close)))
+     (meth POST) (scheme ()) (resource /hello) (version HTTP_1_1)
+     (encoding (Fixed 5))) |}]
   in
   let%bind () = Output_channel.close writer in
-  Input_channel.close reader
+  let%bind () =
+    Pipe.iter_without_pushback reader_pipe ~f:(fun v -> Writer.writef stdout "%S" v)
+  in
+  [%expect {| "HTTP/1.1 200 OK OK\r\ncontent-length: 5\r\n\r\nWorld" |}]
 ;;
