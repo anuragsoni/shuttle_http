353c362208b772180b662b6d254b1b392ec4688b
Anurag Soni <anurag@sonianurag.com>
2022-03-31 22:09:15 -0400

    Update core and async (#20)
    * Update core and async
    
    * Update changelog
    
    * disable on s390x
    
    Reference: https://github.com/ocaml/opam-repository/pull/19740#issuecomment-940076680

--- core/lib/bytebuffer.mli
+++ core/lib/bytebuffer.mli
@@ -11,10 +11,15 @@ val available_to_write : t -> int
 val compact : t -> unit
 val length : t -> int
 val drop : t -> int -> unit
-val read : Unix.File_descr.t -> t -> int
-val read_assume_fd_is_nonblocking : Unix.File_descr.t -> t -> Unix.Syscall_result.Int.t
-val write : Unix.File_descr.t -> t -> int
-val write_assume_fd_is_nonblocking : Unix.File_descr.t -> t -> int
+val read : Core_unix.File_descr.t -> t -> int
+
+val read_assume_fd_is_nonblocking
+  :  Core_unix.File_descr.t
+  -> t
+  -> Core_unix.Syscall_result.Int.t
+
+val write : Core_unix.File_descr.t -> t -> int
+val write_assume_fd_is_nonblocking : Core_unix.File_descr.t -> t -> int
 
 module Fill : sig
   val char : t -> char -> unit
