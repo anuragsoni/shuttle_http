9b3f1c4ab4bc5e36f581b91f98f0626fcdc4f14f
Anurag Soni <anurag@sonianurag.com>
2022-12-25 15:42:27 -0500

    docstrings for request and response

--- http/src/request.mli
+++ http/src/request.mli
@@ -1,3 +1,4 @@
+(** [t] Represents a HTTP 1.1 request. *)
 type t [@@deriving sexp_of]
 
 val create
@@ -8,9 +9,20 @@ val create
   -> string
   -> t
 
+(** [meth] returns the HTTP method of this request. *)
 val meth : t -> Meth.t
+
+(** [path] returns the path component and query parameters of the request URI *)
 val path : t -> string
+
+(** [version] returns the HTTP version number for the request. *)
 val version : t -> Version.t
+
+(** [headers] returns HTTP headers of this request. *)
 val headers : t -> Headers.t
+
+(** [body] returns the body payload of this request. *)
 val body : t -> Body.t
+
+(** [set_body] stores a body payload within the request. *)
 val set_body : t -> Body.t -> unit
--- http/src/response.mli
+++ http/src/response.mli
@@ -1,3 +1,4 @@
+(** [t] Represents a HTTP 1.1 response. *)
 type t [@@deriving sexp_of]
 
 val create
@@ -8,8 +9,17 @@ val create
   -> Status.t
   -> t
 
+(** [version] returns the HTTP version number for the response. *)
 val version : t -> Version.t
+
+(** [status] returns the Status code for this response. *)
 val status : t -> Status.t
+
+(** [reason_phrase] returns the status reason phrase for the response. *)
 val reason_phrase : t -> string
+
+(** [headers] returns the HTTP headers for this response. *)
 val headers : t -> Headers.t
+
+(** [body] returns the body payload of this response. *)
 val body : t -> Body.t
