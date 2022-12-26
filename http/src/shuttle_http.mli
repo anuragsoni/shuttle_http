91809cb4e1f888ab85251fa1ff201c93e91e3bac
Anurag Soni <anurag@sonianurag.com>
2022-12-26 14:23:12 -0500

    More docstrings

--- http/src/shuttle_http.mli
+++ http/src/shuttle_http.mli
@@ -2,13 +2,6 @@ open! Core
 open! Async
 module Headers = Headers
 module Meth = Meth
-module Request = Request
-module Response = Response
-module Server = Server
-module Status = Status
-module Version = Version
-module Parser = Parser
-module Stream_intf = Stream_intf
 
 module Body : sig
   type t [@@deriving sexp_of]
@@ -29,4 +22,74 @@ module Body : sig
 
   (** [to_stream] converts a HTTP body to a stream. *)
   val to_stream : t -> (module Stream_intf.S)
+
+  val stream_of_pipe
+    :  [ `Chunked | `Fixed of int ]
+    -> string Pipe.Reader.t
+    -> (module Stream_intf.S)
 end
+
+module Request : sig
+  (** [t] Represents a HTTP 1.1 request. *)
+  type t [@@deriving sexp_of]
+
+  val create
+    :  ?version:Version.t
+    -> ?headers:Headers.t
+    -> ?body:Body.t
+    -> Meth.t
+    -> string
+    -> t
+
+  (** [meth] returns the HTTP method of this request. *)
+  val meth : t -> Meth.t
+
+  (** [path] returns the path component and query parameters of the request URI *)
+  val path : t -> string
+
+  (** [version] returns the HTTP version number for the request. *)
+  val version : t -> Version.t
+
+  (** [headers] returns HTTP headers of this request. *)
+  val headers : t -> Headers.t
+
+  (** [body] returns the body payload of this request. *)
+  val body : t -> Body.t
+
+  (** [set_body] stores a body payload within the request. *)
+  val set_body : t -> Body.t -> unit
+end
+
+module Response : sig
+  (** [t] Represents a HTTP 1.1 response. *)
+  type t [@@deriving sexp_of]
+
+  val create
+    :  ?version:Version.t
+    -> ?reason_phrase:string
+    -> ?headers:Headers.t
+    -> ?body:Body.t
+    -> Status.t
+    -> t
+
+  (** [version] returns the HTTP version number for the response. *)
+  val version : t -> Version.t
+
+  (** [status] returns the Status code for this response. *)
+  val status : t -> Status.t
+
+  (** [reason_phrase] returns the status reason phrase for the response. *)
+  val reason_phrase : t -> string
+
+  (** [headers] returns the HTTP headers for this response. *)
+  val headers : t -> Headers.t
+
+  (** [body] returns the body payload of this response. *)
+  val body : t -> Body.t
+end
+
+module Server = Server
+module Status = Status
+module Version = Version
+module Parser = Parser
+module Stream_intf = Stream_intf
